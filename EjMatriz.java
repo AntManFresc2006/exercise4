618eeab76614ed5eb2fed173868ac22241d877a8
AntManFresc2006 <antoniomanuelfresco@gmail.com>
2024-11-22 14:01:15 +0100

    Add files via upload

--- EjMatriz.java
+++ EjMatriz.java
@@ -1,4 +1,4 @@
-package ejercicios;
+		package exercise4;
 
 import java.util.Scanner;
 
@@ -6,11 +6,11 @@ public class EjMatriz {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 
-		int opcion, cantidad, texto, total;
-		
-		String cadena = null;
-		// int [][] nuevaMatriz = randoms(filas, columnas);
-
+		int opcion, sumaColumnas =0, sumaFilas=0;
+		String palabraColumnas = "";
+		String palabraFilas = "";
+		String palabraDiagonal = "";
+		String palabraDeterminantes = "";
 		do {
 
 			System.out.println("Pulsa '1' si quieres empezar este programa, pulsa '0' si quieres acabar.");
@@ -22,15 +22,30 @@ public class EjMatriz {
 				
 				System.out.println("Tu tabla es... ");
 				
-				System.out.println(randoms(cadena)); 
+				System.out.println("Dime la cantidad de filas que quiere que tenga tu matriz.");
+				int filas = sc.nextInt();
+				System.out.println("Dime la cantidad de columnas que quiere que tenga tu matriz.");
+				int columnas = sc.nextInt();
+				
+				int[][] matriz = new int[filas][columnas];
+				
+				randoms (filas, columnas, matriz);
+				
+				filasRandoms(matriz);
+				
+				sumaFilas(matriz, columnas, filas, sumaFilas, palabraFilas);
+				
+				System.out.println("Ahora la suma de las columnas");
+				
+				sumaColumnas(matriz, columnas, filas, sumaColumnas, palabraColumnas);
 
-				System.out.println("Fin del programa.");
+				System.out.println("\nFin de la matriz.");
 				break;
 			case 0:
-				System.out.println("Adios.");
+				System.out.println("\nAdios.");
 				break;
 			default:
-				System.out.println("Error, vuelve a intentarlo.");
+				System.out.println("\nError, vuelve a intentarlo.");
 				break;
 			}
 
@@ -39,38 +54,83 @@ public class EjMatriz {
 		sc.close();
 	}
 
-	public static int[][] randoms(String cadena) {
-		Scanner sc = new Scanner(System.in);
-		// Aqui se piden las filas y columnas
-		
-		System.out.println("Dime la cantidad de filas que quiere que tenga tu matriz.");
-		int filas = sc.nextInt();
-		System.out.println("Dime la cantidad de columnas que quiere que tenga tu matriz.");
-		int columnas = sc.nextInt();
-
-		int[][] matriz = new int[filas][columnas];
+	public static int[][] randoms(int filas, int columnas, int[][] matriz) {
 		int max = 100;
 		int min = 0;
 		int range = (max - min) + 1;
 
 		// Este es el for donde la matriz va a llenarse de numeros randoms
 		for (int i = 0; i < columnas; i++) {
-			cadena = "";
+			
 			for (int n = 0; n < filas; n++) {
 				int random = (int) ((range * Math.random()) + min);
 				
-				matriz[n][i]= random;
-				
-				if (n == filas - 1) {
-					cadena +=matriz[n][i];
-				} else {
-					cadena += matriz[n][i] + ", ";
-				}
-
+				matriz[i][n]= random;
 			}
-			System.out.println(" (" + cadena + ")");
+		}
+		
+		return matriz;
+	}
+	
+	public static int[][] filasRandoms(int [][] matriz) {
+		
+		for (int j=0; j<matriz.length; j++) {
+			for (int q=0; q<matriz[j].length;q++) {
+				System.out.print(matriz[j][q]+"\t");
+			}
+			System.out.println("");
 		}
 		return matriz;
 	}
+	
+public static int[][] sumaFilas(int [][] matriz, int columnas, int filas, int sumaFilas, String palabraFilas) {
+		
+		for (int i=0; i<filas; i++) {
+			palabraFilas = "";
+			sumaFilas = 0;
+			for (int n=0; n<columnas; n++) {
+				sumaFilas += matriz[i][n];
+				if (n == filas-1) {
+					palabraFilas += matriz[i][n];
+				}
+				else {
+					palabraFilas +=  matriz[i][n]+" + ";
+					
+				}
+				
+			}
+			
+			System.out.println("\n Fila "+(i+1)+": "+palabraFilas+" = "+sumaFilas);
+			
+		}
+		
+		return matriz;
+	}
+
+public static int[][] sumaColumnas(int [][] matriz, int columnas, int filas, int sumaColumnas, String palabraColumnas) {
+	
+	for (int i=0; i<matriz[columnas-1].length; i++) {
+		System.out.println("\nfila: "+i);
+		palabraColumnas = "";
+		sumaColumnas = 0;
+		for (int n=0; n<matriz.length; n++) {
+			System.out.println("\ncolumna: "+n);
+			sumaColumnas += matriz[n][i];
+			if (n == filas-1) {
+				palabraColumnas += matriz[n][i];
+			}
+			else {
+				palabraColumnas +=  matriz[n][i]+" + ";
+				
+			}
+			
+		}
+		
+		System.out.println("\n Columna "+(i+1)+": "+palabraColumnas+" = "+sumaColumnas);
+		
+	}
+	
+	return matriz;
+}
 
 }
